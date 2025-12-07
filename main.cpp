e8a2b32a7d2937a582813de5d901a36944c599fc
Youssef <sefiani.ilyas2006@gmail.com>
2025-12-07 20:54:26 +0100

    camera gefikst bijna

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #include "Mapreader.h"
 #include "Game.h"
 int main() {
-    Camera camera(2000,1100);
+    Camera camera(1900,1100);
     tuple<float,float> pixel = {1,-1};
     auto world = camera.worldCoToPixelsCo(pixel);
     Mapreader reader("../levelTest.txt");
